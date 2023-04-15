6cc4be91e58b5df51ba172523e983cfeceee9e6b
Ahmed <ahmed91alhaj@gmail.com>
2023-04-14 22:51:05 -0400

    implemented simple timer

---
+++ timer1.js
@@ -0,0 +1,14 @@
+const args = process.argv.slice(2);
+
+for (const arg of args) {
+  const time = parseInt(arg);
+  if (isNaN(arg) || arg < 0) {
+    continue;
+  } else if ([3, 5, 9, 10, 15].includes(time)) {
+    setTimeout(() => {
+      process.stdout.write("Beep! ");
+    }, time * 1000);
+  } else {
+    process.exit();
+  }
+}
