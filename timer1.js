577e40a1a2e31f702489ed76c4c0fdbb4211b7f1
Ahmed <ahmed91alhaj@gmail.com>
2023-04-24 05:31:25 -0400

    Refactor the code to for reusability and modularity

--- timer1.js
+++ timer1.js
@@ -1,12 +1,25 @@
-const args = process.argv.slice(2);
+// Define a function called "timer"
+const timer = function () {
+  // Get the command line arguments starting from the 3rd element (index 2)
+  const args = process.argv.slice(2);
 
-for (const arg of args) {
-  const time = parseInt(arg);
-  if (isNaN(time) || time < 0) {
-    continue;
-  } else if ([3, 5, 9, 10, 15].includes(time)) {
-    setTimeout(() => {
-      process.stdout.write("Beep! ");
-    }, time * 1000);
+  // Loop through each argument
+  for (const arg of args) {
+    // Parse the argument as an integer
+    const time = parseInt(arg);
+
+    // If the argument is not a positive integer, skip to the next argument
+    if (isNaN(time) || time < 0) {
+      continue;
+    }
+    // Otherwise, schedule a beep after the specified amount of time
+    else {
+      setTimeout(() => {
+        process.stdout.write("Beep! ");
+      }, time * 1000);
+    }
   }
-}
+};
+
+// Call the "timer" function to start scheduling beeps
+timer();
